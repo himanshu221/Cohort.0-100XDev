8d3019441885f0bea08c161fd2bbc1edce9dcee5
Himanshu <himanshubhushan775@gmail.com>
2024-01-15 21:54:25 +0530

    on 6.1

--- Week 5/5.2-full-stack-todo-app/README.md
+++
@@ -1,7 +0,0 @@
-# Todo App
-
-This has a simple full stack **TODO Application** which has the following features : 
-
-- Anyone can create a todo.
-- Anyone can see their exisiting todos.
-- Anyone can mark a todo as done.
---
+++ Week 6/6.1-code-along-1/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, memo } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [title, updateTitle] = useState("Himanshu");
+  return (
+    <>
+      <TitleChange updateTitle={updateTitle} />
+      <Header title={title} />
+      <Header title={"Bhushan1"} />
+      <Header title={"Bhushan2"} />
+      <Header title={"Bhushan3"} />
+      <Header title={"Bhushan4"} />
+      <Header title={"Bhushan5"} />
+    </>
+  )
+    
+}
+function TitleChange({updateTitle}){
+  return <button onClick={() => {
+    {updateTitle(Math.random())}
+  }}>Click me to change the title</button>
+}
+// function Header({title}) {
+//   return <div>
+//     {title}
+//   </div>
+// }
+
+const Header = memo(({title}) => {
+    return <div>
+      {title}
+    </div>
+})
+
+export default App
