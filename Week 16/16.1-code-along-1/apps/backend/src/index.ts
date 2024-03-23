229a5d7e5e97adaa71e836ce46a27695ee64099c
Himanshu <himanshubhushan775@gmail.com>
2024-03-23 17:39:28 +0530

    completed week q6

--- Week 16/16.1-code-along-1/apps/backend/src/index.ts
+++ Week 16/16.1-code-along-1/apps/backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import { BACKEND_URL} from '@repo/common/config'
 
 const app = express()
 
 app.get('/health',(req, resp) => {
+    console.log(BACKEND_URL)
     return resp.json({
         message: "healthy"
     })
