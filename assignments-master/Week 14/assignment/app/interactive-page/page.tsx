a81228688614db0e5657d0c68d820d1339b896ad
Himanshu <himanshubhushan775@gmail.com>
2024-03-26 00:36:23 +0530

    updated code

--- assignments-master/Week 14/assignment/app/interactive-page/page.tsx
+++ assignments-master/Week 14/assignment/app/interactive-page/page.tsx
@@ -1,13 +1,25 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
+const arr = ["banana", "apple", ""]
 export default function Interactive() {
   const [count, setCount] = useState(0)
+  const [word, setWord] = useState("")
+
+  useEffect(() => {
+      setTimeout(() => {
+        setWord("Banana")
+      })
+  },[])
     return (
         <div className="h-screen flex justify-center items-center">
           <div className="flex flex-col justify-center">
             <h1>Welcome to Interactive Page</h1>
             <button className="p-3 border-2 mt-3 rounded-lg" onClick={() => setCount(count+1)}>Count is {count}</button>
+          </div>
+          <div>
+            {word}
           </div>
         </div>
     )
