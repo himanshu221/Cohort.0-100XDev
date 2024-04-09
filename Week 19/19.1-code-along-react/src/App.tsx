b8f5616f692ef779c9f3e20c5467db4cee92c8e6
Himanshu <himanshubhushan775@gmail.com>
2024-04-10 01:48:30 +0530

    Added react app as client for websocket week 19

---
+++ Week 19/19.1-code-along-react/src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+
+function App() {
+  const [socket, setSocket] = useState<null | WebSocket>(null)
+
+
+  useEffect(() => {
+      
+  },[])
+  return (
+      <div>
+        Hi there
+      </div>
+  )
+}
+
+export default App
