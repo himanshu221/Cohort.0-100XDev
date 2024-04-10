b177d4758f133da6495eec2751f254bebf4b20a7
Himanshu <himanshubhushan775@gmail.com>
2024-04-10 20:42:23 +0530

    completed week 19

--- Week 19/19.1-code-along-react/src/App.tsx
+++ Week 19/19.1-code-along-react/src/App.tsx
@@ -1,16 +1,43 @@
 import { useEffect, useState } from 'react'
-
-
 function App() {
   const [socket, setSocket] = useState<null | WebSocket>(null)
-
+  const [input, setInput] = useState<string>("")
+  const [messages, setMessages] = useState<string[]>([])
 
   useEffect(() => {
-      
+      const newSocket = new WebSocket('ws://localhost:8000')
+      newSocket.onopen = () => {
+        console.log("Connection to socker established")
+        setSocket(newSocket)
+      }
+      newSocket.onmessage = (message) => {
+        setMessages([...messages, message.data])
+      }
+      return () => newSocket.close()
   },[])
+
+  if(!socket){
+    return <div>
+      Connecting to web socket server ....
+    </div>
+  }
   return (
       <div>
-        Hi there
+        <div>
+          <input type="text" placeholder='Write here' onChange={(e) => {
+            setInput(e.target.value)
+          }}/>
+        </div>
+        <div>
+          <button onClick={() => {
+            socket?.send(input)
+          }}>Send Message</button>
+        </div>
+        {messages.map(ms => {
+          return <div>
+            {ms}
+          </div>
+        })}
       </div>
   )
 }
